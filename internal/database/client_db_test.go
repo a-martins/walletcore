6b1caa3bc779f9cebf507fe5d96080aa7c40f080
Alexandre Martins <alexandremartins.ti@gmail.com>
2024-01-03 14:22:10 -0300

    ClientDB and Unit Tests

---
+++ internal/database/client_db_test.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	"database/sql"
+	"testing"
+
+	"github.com.br/a-martins/fc-ms-wallet/internal/entity"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/suite"
+)
+
+type ClientDBTestSuite struct {
+	suite.Suite
+	db       *sql.DB
+	ClientDB *ClientDB
+}
+
+func (s *ClientDBTestSuite) SetupSuite() {
+	db, err := sql.Open("sqlite3", ":memory:")
+	s.Nil(err)
+	s.db = db
+	db.Exec("CREATE TABLE clients (id VARCHAR(255), name VARCHAR(255), email VARCHAR(255), created_at date)")
+	s.ClientDB = NewClient(db)
+}
+
+func (s *ClientDBTestSuite) TearDownTestSuite() {
+	defer s.db.Close()
+	s.db.Exec("DROP TABLE clients")
+}
+
+func TestClientDBTestSuite(t *testing.T) {
+	suite.Run(t, new(ClientDBTestSuite))
+}
+
+func (s *ClientDBTestSuite) TestGet() {
+	client, _ := entity.NewClient("John Doe", "j@j.com")
+	s.ClientDB.Save(client)
+
+	clientDB, err := s.ClientDB.Get(client.ID)
+	s.Nil(err)
+	s.Equal(client.ID, clientDB.ID)
+	s.Equal(client.Name, clientDB.Name)
+	s.Equal(client.Email, clientDB.Email)
+	s.Equal(client.CreatedAt, clientDB.CreatedAt)
+}
